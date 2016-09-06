9ee66254f7ab2a9bd206c3549853c91dc4b7f812
Cristin Mello <cristianc.mello@gmail.com>
2016-09-06 02:44:07 +0000

    adicionando back-end de Address e alter. em RegisterController e form de criacao de usuario

--- resources/views/modules/content/user/create.blade.php
+++ resources/views/modules/content/user/create.blade.php
@@ -18,26 +18,26 @@
         <div class="col-md-12">
             <div class="input-group">
                 <span class="input-group-addon">*</span>
-                <input type="text" name="name" class="form-control" placeholder="Nome" required="true">
+                <input type="text" value="{{ old('name') }}" name="name" class="form-control" placeholder="Nome" required="true">
             </div>
         </div>
     </div>
-    
+
     <div class="row">
         <div class="col-md-6">
             <div class="input-group">
                 <span class="input-group-addon">*</span>
-                <input type="email" name="email" class="form-control" placeholder="Email" required="true">
+                <input type="email" value="{{ old('email') }}" name="email" class="form-control" placeholder="Email" required="true">
             </div>
         </div>
         <div class="col-md-6">
             <div class="input-group">
                 <span class="input-group-addon">*</span>
-                <input type="tel" name="phone" class="form-control" placeholder="Telefone" required="true">
+                <input type="tel" value="{{ old('phone') }}" name="phone" class="form-control" placeholder="Telefone" required="true">
             </div>
         </div>
     </div>
-    
+
     <div class="row">
         <div class="col-md-6">
             <div class="input-group">
@@ -52,49 +52,49 @@
             </div>
         </div>
     </div>
-    
+
     <h3>Endereço</h3>
     <div class="row">
         <div class="col-md-2">
             <div class="input-group">
                 <span class="input-group-addon" id="basic-addon1">*</span>
-                <input id="user-zipCode" name="zipCode" type="text" class="form-control" placeholder="CEP" required="true">
+                <input id="user-zipCode" value="{{ old('zipCode') }}" name="zipCode" type="text" class="form-control" placeholder="CEP" required="true">
             </div>
         </div>
         <div class="col-md-6">
             <div class="input-group">
                 <span class="input-group-addon" id="basic-addon1">*</span>
-                <input id="user-state" name="state" type="text" class="form-control" placeholder="Estado" required="true">
+                <input id="user-state" value="{{ old('state') }}" name="state" type="text" class="form-control" placeholder="Estado" required="true">
             </div>
         </div>
         <div class="col-md-4">
             <div class="input-group">
                 <span class="input-group-addon" id="basic-addon1">*</span>
-                <input id="user-city" name="city" type="text" class="form-control" placeholder="Cidade" required="true">
+                <input id="user-city" value="{{ old('city') }}" name="city" type="text" class="form-control" placeholder="Cidade" required="true">
             </div>
         </div>
     </div>
-    
+
     <div class="row">
         <div class="col-md-8">
             <div class="input-group">
                 <span class="input-group-addon" id="basic-addon1">*</span>
-                <input id="user-neighborhood" name="neighborhood" type="text" class="form-control" placeholder="Bairro" required="true">
+                <input id="user-neighborhood" value="{{ old('neighborhood') }}" name="neighborhood" type="text" class="form-control" placeholder="Bairro" required="true">
             </div>
         </div>
         <div class="col-md-4">
             <div class="input-group">
                 <span class="input-group-addon" id="basic-addon1">*</span>
-                <input id="user-additionalData" name="additionalData" type="text" class="form-control" placeholder="Complemento" required="true">
+                <input id="user-additionalData" value="{{ old('additionalData') }}" name="additionalData" type="text" class="form-control" placeholder="Complemento">
             </div>
         </div>
     </div>
-    
+
     <div class="row">
         <div class="col-md-12">
             <div class="input-group">
                 <span class="input-group-addon" id="basic-addon1">*</span>
-                <input id="user-street" name="street" type="text" class="form-control" placeholder="Endereço" required="true">
+                <input id="user-street" value="{{ old('street') }}" name="street" type="text" class="form-control" placeholder="Endereço" required="true">
             </div>
         </div>
     </div>
