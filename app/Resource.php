34553720a0f8a6c32dfa2a8e16b6df1a7365c366
Cristin Mello <cristianc.mello@gmail.com>
2016-09-06 20:12:22 +0000

    adicionando backend de Category e Resource (Tables e Models)

---
+++ app/Resource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Resource extends Model
+{
+    // Campos que permitirão Mass Assignment
+    protected $fillable = [
+        "name",
+        "technicalDescription",
+        "informalDescription",
+        "uriResources",
+        "user_id"
+    ];
+
+    // Promover o relacionamento (Resource -> Category)
+    public function categories()
+    {
+        // Resource possui várias categorias
+        return $this->belongsToMany('App\Category', 'resources_categories');
+    }
+
+    // Promover o relacionamento (Resource -> User)
+    public function user()
+    {
+        // Resource possui 1 usuário
+        return $this->belongsTo('App\User');
+    }
+}
---
+++ database/migrations/2016_09_06_191748_create_categories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id')->index();
+
+            // Campos não-nulos e nome não pode repetir
+            $table->string("name")->unique();               // não-nulo
+            $table->string("description", 1000);            // não-nulo
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
