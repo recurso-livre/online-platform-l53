40e7d9c41a02c989bf5627135f1aeb3be99452f7
Emerson Capuchi Romaneli <emerson.romaneli@uvvnet.com.br>
2016-09-19 21:31:05 +0000

    Adicionando traduções

--- resources/views/modules/header/navbar.blade.php
+++ resources/views/modules/header/navbar.blade.php
@@ -2,17 +2,17 @@
   <div class="container">
     <!-- Brand and toggle get grouped for better mobile display -->
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#mobile-navbar" aria-expanded="false">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="/"><img class="rl-logo" src="{{ asset('assets/img/logo/rl-logo-white.png') }}"></img></a>
+      <a class="navbar-brand" style="width: 216px;" href="/"><img class="rl-logo" src="{{ asset('assets/img/logo/rl-logo-white.png') }}"></img></a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <div class="collapse navbar-collapse" id="mobile-navbar">
       <ul class="nav navbar-nav">
         <form id="search-form" class="navbar-form navbar-left">
           <div class="input-group navbar-search">
@@ -50,7 +50,6 @@
             </a>
           </li>
         @endif
-        
       </ul>
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
@@ -60,17 +59,19 @@
 @if (Auth::guest())
   <!-- Modal -->
   <div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="login" aria-hidden="true">
-          @if($errors->has('email'))
-            <div class="alert alert-danger modal-dialog modal-sm" role="alert">
-          		<ul>
-          			<li>{{ $errors->first('email') }}</li>
-          		</ul>
-          	</div>
-          @endif
+    @if($errors->has('email'))
+      <div class="alert alert-danger modal-dialog modal-sm" role="alert">
+    		<ul>
+    			@foreach($errors->all() as $error)
+    				<li>{{ $error }}</li>
+    			@endforeach
+    		</ul>
+    	</div>
+    @endif
     <div class="modal-dialog modal-sm" role="document">
       <div class="modal-content">
         
-        <form method="post" action="{{ route('guest.login.post') }}">
+        <form method="post" name="login" action="{{ route('guest.login.post') }}">
           {{ csrf_field() }}
           <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -80,13 +81,13 @@
           </div>
           <div class="modal-body">
             <div class="row">
-              <div class="col-md-12" style="margin-bottom: 8px">
+              <div class="col-md-12"  style="margin-bottom: 8px">
                 <label>Email</label>
-                <input type="email" name="email" class="form-control" placeholder="Email" required/>
+                <input type="email" name="email" class="form-control" placeholder="Email" regex-pattern="[^@]*@.*" regex-show-class="form-control" required />
               </div>
               <div class="col-md-12">
                 <label>Senha</label>
-                <input type="password" name="password" class="form-control" placeholder="Senha"  required/>
+                <input type="password" name="password" class="form-control" placeholder="Senha" required />
               </div>
             </div>
             <div class="row">
